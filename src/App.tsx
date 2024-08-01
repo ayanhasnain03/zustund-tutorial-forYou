710b1f79e89c1ab8076c3cc3c71d863d635b6c20
Ayan <ayan2572006@gmail.com>
2024-08-01 14:17:49 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button className="text-red-500">Click me</Button>
+    </div>
+  );
+};
+export default App;
